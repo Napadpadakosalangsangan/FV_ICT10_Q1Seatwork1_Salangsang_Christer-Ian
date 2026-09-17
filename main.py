5540883e7e72664e8e31abf6544a2af83c76e101
Napadpadakosalangsangan <salangsang.ca@obmontessori.edu.ph>
2026-09-17 20:13:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ age = 16
 he1ght = 170.18
 
 # List
-countries_to_visit = ["France", "Italy", "Netherlands"]
+countries_to_visit = [" France ", " Italy ", " Netherlands "]
 
 # Boolean
 student_type = False
@@ -19,21 +19,44 @@ student_type = False
 favorites = {'color':'Light Blue', 'Car_brand':'a', 'shoe_size':'9.5 US', 'Best_friend':'Ethan'}
 
 # Set
-fruits = set(["Apples", "Oranges", "Watermelons"])
+fruits = set([" Apples ", " Oranges ", " Watermelons "])
 
 # Tuple
-day_ranks = ("Friday", "Saturday", "Sunday", "Thursday", "Tuesday", "Wednesday", "Monday")
+day_ranks = (" Friday ", " Saturday ", " Sunday ", " Thursday ", " Tuesday ", " Wednesday ", " Monday ")
 
-display(f"My name is {name}. I am currently {age} years old, with a height of {he1ght}cm. In the future, I would like to visit the following countries: {str(countries_to_visit)}. You think I'm a new student? {student_type}! Some of my preferences are: {favorites}, and my favorite fruits are {fruits}. Based on my personal experiences, I would rank the days in the week from best to boring in this order: {day_ranks}.", target="div1")
+display(f"My name is {name}. I am currently {age} years old, with a height of {he1ght}cm. In the future, I would like to visit the following countries: {",".join(countries_to_visit)}. You think I'm a new student? {student_type}! Some of my preferences are: , and my favorite fruits are {",".join(fruits)}. Based on my personal experiences, I would rank the days in the week from best to boring in this order: {",".join(day_ranks)}.", target="div1")
 
 
 
 def add(e):
 
-  num1 = float(document.getElementById("num1"))
-  num2 = float(document.getElementById("num2"))
+  num1 = float(document.getElementById("num1").value)
+  num2 = float(document.getElementById("num2").value)
 
-  document.GetElementById("adding").innerTEXT = num1 + num2
+  display(num1 + num2, target = "adding")
+
+def subtract(e):
+
+  num1 = float(document.getElementById("num1").value)
+  num2 = float(document.getElementById("num2").value)
+
+  display(num1 - num2, target = "subtracting")
+
+def multiply(e):
+
+  num1 = float(document.getElementById("num1").value)
+  num2 = float(document.getElementById("num2").value)
+
+  display(num1 * num2, target = "multiplying")
+
+def divide(e):
+
+  num1 = float(document.getElementById("num1").value)
+  num2 = float(document.getElementById("num2").value)
+
+  display(num1 / num2, target = "dividing")
+
+
 
   
 
